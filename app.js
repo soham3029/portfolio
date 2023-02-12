872010408e3ab9a6cec26b650e544bdab763efd8
soham3029 <sohammahapatracse@gmail.com>
2023-02-12 22:13:27 +0530

    First commit

---
+++ app.js
@@ -0,0 +1,32 @@
+const sections = document.querySelectorAll('.section');
+const sectBtns = document.querySelectorAll('.controlls');
+const sectBtn = document.querySelectorAll('.control');
+const allSections = document.querySelector('.main-content');
+
+function pageTransition(){
+    for(let i = 0; i < sectBtn.length; i++){
+        sectBtn[i].addEventListener('click', function(){
+            let currentButton = document.querySelectorAll('.active-btn');
+            currentButton[0].className = currentButton[0].className.replace('active-btn', '');
+            this.className += ' active-btn';
+        })
+    }
+
+    allSections.addEventListener('click', e=>{
+        const id = e.target.dataset.id;
+        if(id){
+            sectBtns.forEach((btn) => {
+                btn.classList.remove('active');
+            })
+            e.target.classList.add('active')
+
+            sections.forEach((section) =>{
+                section.classList.remove('active')
+            })
+        }
+        const element = document.getElementById(id);
+        element.classList.add('active');
+    })
+};
+
+pageTransition();
